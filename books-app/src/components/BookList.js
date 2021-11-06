1fd789449a612571dbfdf7935df360efad4794bc
damladlg <damlabileydi@gmail.com>
2021-11-06 14:59:23 +0300

    Props drilling convert to context api with provider, consumer

--- books-app/src/components/BookList.js
+++ books-app/src/components/BookList.js
@@ -1,24 +1,29 @@
 import React from 'react';
+import { BookContext } from '../contexts/BookContext';
 import Book from './Book';
 import './BookList.css';
 class BookList extends React.Component {
     render() {
-        const bookList = this.props.books.map((book, key) => {
-            return <Book book={book}
-                key={key} />
-        })
+
         return (
-            <section className="page-section bg-light" id="portfolio">
-                <div className="container">
-                    <div className="text-center">
-                        <h2 className="section-heading text-uppercase">Bookfolio</h2>
-                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                    </div>
-                    <div className="row">
-                        {bookList}
-                    </div>
-                </div>
-            </section>
+            <BookContext.Consumer>
+                {value => {
+                    {/* consumer ile contexden provider ile gönderilen valuedan veriyi aldık */ }
+                    return (
+                        <section className="page-section bg-light" id="portfolio">
+                            <div className="container">
+                                <div className="text-center">
+                                    <h2 className="section-heading text-uppercase">Bookfolio</h2>
+                                    <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                                </div>
+                                <div className="row">
+                                    {value.books.map((book, i) => { return <Book book={book} key={i} /> })}
+                                </div>
+                            </div>
+                        </section>
+                    )
+                }}
+            </BookContext.Consumer>
         )
     }
 }
