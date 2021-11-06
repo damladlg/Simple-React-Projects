828c085167d993c7a5d65d77cd2b2e7002cf5052
damladlg <damlabileydi@gmail.com>
2021-11-06 13:12:11 +0300

    created bookfolio page.

--- books-app/src/components/Book.js
+++ books-app/src/components/Book.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import './Book.css';
 class Book extends React.Component {
     render() {
 
         return (
-            <div><h2>{this.props.book.title}</h2></div>
+            <div className="col-lg-4 col-sm-6 mb-4">
+
+                <div className="portfolio-item">
+                    <a className="portfolio-link" data-bs-toggle="modal" href="#portfolioModal1">
+                        <div className="portfolio-hover">
+                            <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+                        </div>
+                        <img className="img-fluid" src={this.props.book.imageURL} alt="..." />
+                    </a>
+                    <div className="portfolio-caption">
+                        <div className="portfolio-caption-heading">{this.props.book.title}</div>
+                        <div className="portfolio-caption-subheading text-muted">{this.props.book.author}</div>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
 export default Book;
+
--- books-app/src/components/BookList.js
+++ books-app/src/components/BookList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Book from './Book';
-
+import './BookList.css';
 class BookList extends React.Component {
     render() {
         const bookList = this.props.books.map((book, key) => {
@@ -8,8 +8,19 @@ class BookList extends React.Component {
                 key={key} />
         })
         return (
-            <div>{bookList}</div>
+            <section className="page-section bg-light" id="portfolio">
+                <div className="container">
+                    <div className="text-center">
+                        <h2 className="section-heading text-uppercase">Bookfolio</h2>
+                        <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    </div>
+                    <div className="row">
+                        {bookList}
+                    </div>
+                </div>
+            </section>
         )
     }
 }
 export default BookList;
+
