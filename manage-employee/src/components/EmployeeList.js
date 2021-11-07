dd980ce396f36b98e7db49da53af0831e5d158b1
damladlg <damlabileydi@gmail.com>
2021-11-07 15:42:50 +0300

    created modal-form to add a new employee.

--- manage-employee/src/components/EmployeeList.js
+++ manage-employee/src/components/EmployeeList.js
@@ -1,24 +1,59 @@
 import Employee from "./Employee";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { EmployeeContext } from "../contexts/EmployeeContext";
+import { Button, Modal } from 'react-bootstrap'
+import AddForm from "./AddForm";
+
 
 const EmployeeList = () => {
 
     const { employees } = useContext(EmployeeContext);
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
-        <table className="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody> <Employee employees={employees} /></tbody>
-        </table>
+        <>
+            <div className="table-title">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h2>Manage <b>Employees</b></h2>
+                    </div>
+                    <div className="col-sm-6">
+                        <Button onClick={handleShow} className="btn btn-success text-white" data-toggle="modal"><i className="material-icons">&#xE147;</i> <span>Add New Employee</span></Button>
+                    </div>
+                </div>
+            </div>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody> <Employee employees={employees} /></tbody>
+            </table>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header className="modal-header" closeButton>
+                    <Modal.Title>
+                        Add Employee
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AddForm />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={handleClose} variant="secondary">
+                        Close Modal
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
     )
 }
 
