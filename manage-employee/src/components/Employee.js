564837ab8ccaaf7f3a5bcde3dc4c63e96f1f7d2b
damladlg <damlabileydi@gmail.com>
2021-11-07 12:24:30 +0300

    used useState hook.

--- manage-employee/src/components/Employee.js
+++ manage-employee/src/components/Employee.js
@@ -1,15 +1,22 @@
-const Employee = () => {
+const Employee = ({ employees /*  props yakalandı*/ }) => {
     return (
-        <tr>
-            <td>Thomas Hardy</td>
-            <td>thomashardy@mail.com</td>
-            <td>89 Chiaroscuro Rd, Portland, USA</td>
-            <td>(171) 555-2222</td>
-            <td>
-                <a href="#editEmployeeModal" class="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                <a href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-            </td>
-        </tr>
+        <>
+            {
+                employees.map((employee) => (
+                    <tr key={employee.id}>
+                        <td>{employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.address}</td>
+                        <td>{employee.phone}</td>
+                        <td>
+                            <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+                            <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                        </td>
+                    </tr>
+                ))
+            }
+        </>
+
     )
 }
 
