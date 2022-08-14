658c0f267aa5e5c435395c3e2a03788ab745c587
Damla Dalgıç <53597482+damladlg@users.noreply.github.com>
2022-08-14 23:53:45 +0300

    Moved a new repo.

--- obss-frontend-project-dd/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./NavBar";
-import Footer from "./Footer";
-import Banner from "./Banner";
-import PopularTags from "./PopularTags";
-import LoginPage from "./LoginPage";
-import Profile from "./Profile";
-import HomePage from "./HomePage";
-
-export { Navbar, Footer, Banner, PopularTags, LoginPage, Profile, HomePage };
